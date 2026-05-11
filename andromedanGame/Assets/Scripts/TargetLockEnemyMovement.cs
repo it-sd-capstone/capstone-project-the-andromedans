aa2fbbb01f994c6f29cc139d455f98bded965e7d
Don Valois <dvalois@student.cvtc.edu>
2026-05-11 09:48:46 -0600

    Fixed another spawner issue

--- andromedanGame/Assets/Scripts/TargetLockEnemyMovement.cs
+++ andromedanGame/Assets/Scripts/TargetLockEnemyMovement.cs
@@ -16,14 +16,14 @@ public class TargetLockEnemyMovement : MonoBehaviour
     private bool entered = false;
     [SerializeField]
     public float targetY = 3f;
-
+    private float sineTimer;
     private Vector3 startPosition;
     //private float direction = 1f;
 
-    private void Start()
+    /*private void Start()
     {
         startPosition = transform.position;
-    }
+    }*/
 
     private void Update()
     {
@@ -33,8 +33,9 @@ public class TargetLockEnemyMovement : MonoBehaviour
         }
         else
         {
-            float horizontalOffset = Mathf.PingPong(Time.time * speed, distance * 2) - distance;
-            float verticalOffset = Mathf.Sin(Time.time * frequency) * amplitude;
+            sineTimer += Time.deltaTime;
+            float horizontalOffset = Mathf.PingPong(sineTimer * speed, distance * 2) * distance;
+            float verticalOffset = Mathf.Sin(sineTimer * frequency) * amplitude;
             transform.position = startPosition + new Vector3(horizontalOffset, verticalOffset, 0f);
         }
         
@@ -42,13 +43,17 @@ public class TargetLockEnemyMovement : MonoBehaviour
 
     private void MoveInFrame()
     {
-        if (transform.position.y > targetY)
+        float newY = transform.position.y - verticalSpeed * Time.deltaTime;
+        transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+
+        if (newY <= targetY)
         {
-            transform.position += Vector3.down * verticalSpeed * Time.deltaTime;
-        }
-        else
-        {
+            newY = targetY;
             entered = true;
+            startPosition = new Vector3(transform.position.x, targetY, transform.position.z);
+            sineTimer = 0f;
+
         }
+        transform.position = new Vector3(transform.position.x, newY, transform.position.z);
     }
 }
